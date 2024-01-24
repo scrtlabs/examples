13628a8c6ad256ab426e945605868ceb6974231c
writersblockchain <seanmconrad94@gmail.com>
2024-01-24 13:31:31 -0500

    fetch proposal id

--- evm-confidential-voting/polygon/scripts/vote.js
+++ evm-confidential-voting/polygon/scripts/vote.js
@@ -4,13 +4,15 @@ const { encrypt } = require("./encrypt");
 const { getProposalById } = require("./query_by_proposal_id.js");
 require("dotenv").config();
 
-const privateVotingAddress = process.env.CONTRACT_ADDRESS; // Replace with your deployed contract's address
+const privateVotingAddress = process.env.CONTRACT_ADDRESS; 
 
 async function vote() {
+  let proposal = await getProposalById(1);
+
   let msg = {
     answer: "yes",
-    proposal_id: 1,
-    proposal_description: "Do you like turtles?",
+    proposal_id: proposal.id,
+    proposal_description: proposal.description,
     salt: Math.random(),
   };
   console.log(msg);
@@ -18,7 +20,7 @@ async function vote() {
   let PrivateVoting = await hre.ethers.getContractFactory("PrivateVoting");
   const privateVoting = await PrivateVoting.attach(privateVotingAddress);
 
-  const tx = await privateVoting.vote(1, my_encrypted_message);
+  const tx = await privateVoting.vote(proposal.id, my_encrypted_message);
 
   console.log(`Transaction hash: ${tx.hash}`);
   await tx.wait();
@@ -26,26 +28,3 @@ async function vote() {
   console.log("vote function executed successfully!");
 }
 vote();
-
-// async function vote() {
-//   let proposal = await getProposalById(2);
-
-//   let msg = {
-//     answer: "yes",
-//     proposal_id: proposal.id,
-//     proposal_description: proposal.description,
-//     salt: Math.random(),
-//   };
-//   console.log(msg);
-//   let my_encrypted_message = await encrypt(msg);
-//   let PrivateVoting = await hre.ethers.getContractFactory("PrivateVoting");
-//   const privateVoting = await PrivateVoting.attach(privateVotingAddress);
-
-//   const tx = await privateVoting.vote(proposal.id, my_encrypted_message);
-
-//   console.log(`Transaction hash: ${tx.hash}`);
-//   await tx.wait();
-
-//   console.log("vote function executed successfully!");
-// }
-// vote();
